cf3e6778de30cbb2f8682215a66fa57fc767c6d4
extrude575757 <nicholas.a.lacapria@gmail.com>
2021-01-14 17:52:00 -0800

    On PUT updating user

--- api/server.js
+++ api/server.js
@@ -10,65 +10,8 @@ server.use(express.json());
 
 /*
 Endpoint Specifications
-When the client makes a POST request to /api/users:
 
-If the request body is missing the name or bio property:
 
-respond with HTTP status code 400 (Bad Request).
-return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
-If the information about the user is valid:
-
-save the new user the the database.
-respond with HTTP status code 201 (Created).
-return the newly created user document.
-If there's an error while saving the user:
-
-respond with HTTP status code 500 (Server Error).
-return the following JSON object: { errorMessage: "There was an error while saving the user to the database" }.
-When the client makes a GET request to /api/users:
-
-If there's an error in retrieving the users from the database:
-respond with HTTP status code 500.
-return the following JSON object: { errorMessage: "The users information could not be retrieved." }.
-When the client makes a GET request to /api/users/:id:
-
-If the user with the specified id is not found:
-
-respond with HTTP status code 404 (Not Found).
-return the following JSON object: { message: "The user with the specified ID does not exist." }.
-If there's an error in retrieving the user from the database:
-
-respond with HTTP status code 500.
-return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
-When the client makes a DELETE request to /api/users/:id:
-
-If the user with the specified id is not found:
-
-respond with HTTP status code 404 (Not Found).
-return the following JSON object: { message: "The user with the specified ID does not exist." }.
-If there's an error in removing the user from the database:
-
-respond with HTTP status code 500.
-return the following JSON object: { errorMessage: "The user could not be removed" }.
-When the client makes a PUT request to /api/users/:id:
-
-If the user with the specified id is not found:
-
-respond with HTTP status code 404 (Not Found).
-return the following JSON object: { message: "The user with the specified ID does not exist." }.
-If the request body is missing the name or bio property:
-
-respond with HTTP status code 400 (Bad Request).
-return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
-If there's an error when updating the user:
-
-respond with HTTP status code 500.
-return the following JSON object: { errorMessage: "The user information could not be modified." }.
-If the user is found and the new information is valid:
-
-update the user document in the database using the new information sent in the request body.
-respond with HTTP status code 200 (OK).
-return the newly updated user document.
 */
 // Endpoints
 // [GET] /
@@ -78,10 +21,36 @@ Method	URL	Description
 POST	/api/users	Creates a user using the information 
 sent inside the request body.
 */
+/*
+When the client makes a POST request to /api/users:
+
+If the request body is missing the name or bio property:
+
+respond with HTTP status code 400 (Bad Request).
+return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
+
+If the information about the user is valid:
+
+save the new user the the database.
+respond with HTTP status code 201 (Created).
+return the newly created user document.
+If there's an error while saving the user:
+
+respond with HTTP status code 500 (Server Error).
+return the following JSON object: { errorMessage: "There was an error while saving the user to the database" }.
+
+When the client makes a GET request to /api/users:
+If there's an error in retrieving the users from the database:
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The users information could not be retrieved." }.
+
+*/
 server.post('/api/users', async(req,res) =>{
     const user = req.body;
-    if(!user.name){
-        res.status(400).json({message:'your name is required'})
+    if(!user.name | !user.bio){
+        res.status(400).json({
+            errorMessage: "Please provide name and bio for the user."
+                })
     } else{
             // Database Reactions
         try{
@@ -90,11 +59,17 @@ server.post('/api/users', async(req,res) =>{
             res.status(201).json(theUser);
         } catch (error){
             // Error control
-            res.status(500).json({message:error.message})
+            res.status(500).json({ errorMessage: "The users information could not be retrieved." })
         } 
     } 
 
 });
+
+
+
+
+
+
 
 /*
 GET	/api/users	Returns an array users.
@@ -113,35 +88,56 @@ server.get('/api/users', (req,res) =>{
 /*
 GET	/api/users/:id	Returns the user object with the
 specified id.
+
+When the client makes a GET request to /api/users/:id:
+If the user with the specified id is not found:
+respond with HTTP status code 404 (Not Found).
+return the following JSON object: { message: "The user with the specified ID does not exist." }.
+
+If there's an error in retrieving the user from the database:
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The user information could not be retrieved." }.
+
 */
 server.get('/api/users/:id', (req,res)=>{
     const {id} = req.params;
     User.findById(id)
         .then(user =>{
             if(!user){
-                res.status(404).json({message: `user with id ${id} not found`})
+                res.status(404).json({message: "The user with the specified ID does not exist."})
             }else {
                 res.status(200).json(user)
             }
         })
         .catch(error =>{
-            res.status(500).json({message: error.message})
+            res.status(500).json({errorMessage: "The user information could not be retrieved."})
         })
 })
 // DELETE	/api/users/:id	Removes the user with the specified 
     // id and returns the deleted user.
+    /*
+When the client makes a DELETE request to /api/users/:id:
+If the user with the specified id is not found:
+respond with HTTP status code 404 (Not Found).
+return the following JSON object: { message: "The user with the specified ID does not exist." }.
+
+If there's an error in removing the user from the database:
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The user could not be removed" }.
+
+    */
 server.delete('/api/users/:id', (req,res) =>{
     const { id} = req.params;
     User.delete(id)
         .then(deleted => {
             if (!deleted){
-                res.status(404).json({message: `user with id ${id} not found`})
+                res.status(404).json({message: "The user with the specified ID does not exist."})
             }else {
                 res.status(200).json(deleted)
             }
         })
         .catch( error =>{
-            res.status(500).json({ message: error.message})
+            res.status(500).json({errorMessage: "The user could not be removed"})
         }) 
 })
 
@@ -150,6 +146,24 @@ server.delete('/api/users/:id', (req,res) =>{
 /* 
 PUT	/api/users/:id	Updates the user with the specified id 
 using data from the request body. Returns the modified user
+
+When the client makes a PUT request to /api/users/:id:
+If the user with the specified id is not found:
+respond with HTTP status code 404 (Not Found).
+return the following JSON object: { message: "The user with the specified ID does not exist." }.
+
+If the request body is missing the name or bio property:
+respond with HTTP status code 400 (Bad Request).
+return the following JSON response: { errorMessage: "Please provide name and bio for the user." }.
+
+If there's an error when updating the user:
+respond with HTTP status code 500.
+return the following JSON object: { errorMessage: "The user information could not be modified." }.
+
+If the user is found and the new information is valid:
+update the user document in the database using the new information sent in the request body.
+respond with HTTP status code 200 (OK).
+return the newly updated user document.
 */
 server.put('/api/users/:id', async (req,res) =>{
     const id = req.params.id;
